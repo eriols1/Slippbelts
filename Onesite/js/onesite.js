bc0572e1d6012c63ee95b039e87d7fc32459d85d
eriols1 <Eldopro@outlook.com>
2020-03-02 10:00:48 +0100

    hagj

--- Onesite/js/onesite.js
+++ Onesite/js/onesite.js
@@ -31,3 +31,86 @@ $("#info").click(function(){
     scrollTop: $("#box4").offset().top
   }, 1000);
 });
+/*jslint browser: true*/ /*global  $*/
+$(".button1").show();
+$("#bilddub").show();
+$(".button2").show();
+$(".punkt1").show();
+$(".punkt2").show();
+$(".punkt3").show();
+$(".scroll").show();
+$("#window").css("overflow", "hidden");
+var x = -1;
+var i;
+
+$(".button1").click(function(){
+  x=x+201;
+
+  if (x>600) {
+    x = 0;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$(".button2").click(function(){
+  x=x-201;
+
+  if (x<0) {
+    x=402;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$("#imgs").click(function(){
+  x=x+201;
+
+  if (x>600) {
+    x = 0;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$(".punkt1").click(function(){
+  if (x>0) {
+    x=x-x;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$(".punkt2").click(function(){
+  if (x<1) {
+    x=x+201;
+
+  }else if (x>300) {
+    x=x-201;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$(".punkt3").click(function(){
+  if (x<1) {
+    x=x+402;
+
+  }else if (x>200) {
+    x=x+201;
+  }
+
+  $("#imgs").animate({left:-x},750);
+});
+
+$(".scroll").click(function(){
+  for(i=0;i<306;i+=1){
+    x=x+2;
+
+    if (x>610){
+      x=0;
+    }
+
+    $("#imgs").animate({left:-x},10);
+  }
+});
