9c25010b4d702b32ee580889e30ce3ffc8076f92
eriols1 <Eldopro@outlook.com>
2020-05-20 14:20:39 +0200

    massa saker

--- Backeblock/js/js.js
+++ Backeblock/js/js.js
@@ -1,66 +1,64 @@
 /*jslint browser: true*/ /*global  $*/
-$(".button1").show();
-$(".button2").show();
-$("#window").css("overflow", "hidden");
-var x = -1;
-var i;
+$("nav").click(function () {
+  $("nav").show();
+  $("#dropdown").toggle("slow", function () {
 
-$(".button1").click(function(){
-  x=x+401;
-
-  if (x>1600) {
-    x = 0;
-  }
-
-  $("#imgs").animate({left:-x},0);
+  });
 });
+$("#dots").show();
+$("#window").show();
+$("#noJsImgs").hide();
+$(document).ready(function() {
+  $("#window").css("overflow", "hidden");
+  var x = -1;
 
-$(".dot2").click(function(){
-  x=x-401;
+  $("#imgs").click(function(){
+    x=x+404;
 
-  if (x<0) {
-    x=802;
-  }
+    if (x>2000) {
+      x = 0;
+      $("#imgs").animate({left:-x},0);
+      x=x+404;
+    }
 
-  $("#imgs").animate({left:-x},0);
-});
+    $("#imgs").animate({left:-x},500);
+  });
 
-$("#imgs").click(function(){
-  x=x+403;
+  $("#dot1").click(function(){
+    if (x>0) {
+      x=x-x-2;
+    }
 
-  if (x>1600) {
-    x = 0;
-  }
+    $("#imgs").animate({left:-x},0);
+  });
 
-  $("#imgs").animate({left:-x},500);
-});
+  $("#dot2").click(function(){
+    if (x<1) {
+      x=x+401;
+    }else if (x>300) {
+      x=x-x+401;
+    }
 
-$(".dot").click(function(){
-  if (x>0) {
-    x=x-x;
-  }
+    $("#imgs").animate({left:-x},0);
+  });
 
-  $("#imgs").animate({left:-x},0);
-});
+  $("#dot3").click(function(){
+    if (x<1) {
+      x=x+806;
+    } else if (x>300) {
+      x=x-x+805;
+    }
 
-$(".dot2").click(function(){
-  if (x<1) {
-    x=x+401;
+    $("#imgs").animate({left:-x},0);
+  });
 
-  }else if (x>750) {
-    x=x-401;
-  }
+  $("#dot4").click(function(){
+    if (x<1) {
+      x=x+1209;
+    }else if (x>300) {
+      x=x-x+1209;
+    }
 
-  $("#imgs").animate({left:-x},0);
-});
-
-$(".dot3").click(function(){
-  if (x<1) {
-    x=x+802;
-
-  }else if (x>400) {
-    x=x+401;
-  }
-
-  $("#imgs").animate({left:-x},0);
+    $("#imgs").animate({left:-x},0);
+  });
 });
