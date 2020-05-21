d9e665b634e062297669b4ef073450c829f87def
eriols1 <Eldopro@outlook.com>
2020-05-21 13:35:11 +0200

    resp

--- Backeblock/js/js.js
+++ Backeblock/js/js.js
@@ -26,38 +26,61 @@ $(document).ready(function() {
 
   $("#dot1").click(function(){
     if (x>0) {
-      x=x-x-2;
+      x=x-x;
     }
 
     $("#imgs").animate({left:-x},0);
   });
 
   $("#dot2").click(function(){
-    if (x<1) {
-      x=x+401;
-    }else if (x>300) {
-      x=x-x+401;
-    }
+      if ($(window).width() > 434) {
+        if (x<1) {
+          x=x+401;
+        } else if (x>300) {
+          x=x-x+400;
+        }
+      } else {
+        if (x<1) {
+          x=x+201;
+        } else if (x>150){
+          x=x-x+200;
+        }
+      }
 
     $("#imgs").animate({left:-x},0);
   });
 
   $("#dot3").click(function(){
+    if ($(window).width() > 434) {
     if (x<1) {
-      x=x+806;
+      x=x+805;
     } else if (x>300) {
-      x=x-x+805;
+      x=x-x+804;
     }
-
+  } else {
+      if (x<1) {
+        x=x+405;
+      } else if (x>150){
+        x=x-x+404;
+      }
+}
     $("#imgs").animate({left:-x},0);
   });
 
   $("#dot4").click(function(){
+      if ($(window).width() > 434) {
     if (x<1) {
       x=x+1209;
     }else if (x>300) {
-      x=x-x+1209;
+      x=x-x+1208;
     }
+  } else {
+        if (x<1) {
+          x=x+609;
+        } else if (x>150){
+          x=x-x+608;
+        }
+  }
 
     $("#imgs").animate({left:-x},0);
   });
