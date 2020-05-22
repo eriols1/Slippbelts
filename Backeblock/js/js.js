e94f178162e64244152333165447d0679262e3f0
eriols1 <Eldopro@outlook.com>
2020-05-22 16:47:38 +0200

    changes

--- Backeblock/js/js.js
+++ Backeblock/js/js.js
@@ -1,4 +1,5 @@
 /*jslint browser: true*/ /*global  $*/
+
 $("nav").click(function () {
   $("nav").show();
   $("#dropdown").toggle("slow", function () {
@@ -8,8 +9,23 @@ $("nav").click(function () {
 $("#text2").show();
 $("#dots").show();
 $("#window").show();
+$("#myImg").show();
 $("#noJsImgs").hide();
 $("#noJsLinks").hide();
+if ($(window).width() <= 683) {
+  $("#myImg2").show();
+  $("#myImg").hide();
+}
+window.addEventListener("resize", function () {
+  if ($(window).width() <= 683) {
+    $("#myImg2").show();
+    $("#myImg").hide();
+  }
+  if ($(window).width() > 683) {
+    $("#myImg").show();
+    $("#myImg2").hide();
+  }
+});
 $(document).ready(function() {
   $("#window").css("overflow", "hidden");
   var x = -1;
@@ -23,55 +39,73 @@ $(document).ready(function() {
   });
 
   $("#dot2").click(function(){
-      if ($(window).width() > 434) {
-        if (x<1) {
-          x=x+401;
-        } else if (x>300) {
-          x=x-x+400;
-        }
-      } else {
-        if (x<1) {
-          x=x+201;
-        } else if (x>150){
-          x=x-x+200;
-        }
+    if ($(window).width() > 434) {
+      if (x<1) {
+        x=x+401;
+      } else if (x>300) {
+        x=x-x+400;
       }
+    } else {
+      if (x<1) {
+        x=x+201;
+      } else if (x>150){
+        x=x-x+200;
+      }
+    }
 
     $("#imgs").animate({left:-x},0);
   });
 
   $("#dot3").click(function(){
     if ($(window).width() > 434) {
-    if (x<1) {
-      x=x+805;
-    } else if (x>300) {
-      x=x-x+804;
-    }
-  } else {
+      if (x<1) {
+        x=x+805;
+      } else if (x>300) {
+        x=x-x+804;
+      }
+    } else {
       if (x<1) {
         x=x+405;
       } else if (x>150){
         x=x-x+404;
       }
-}
+    }
     $("#imgs").animate({left:-x},0);
   });
 
   $("#dot4").click(function(){
-      if ($(window).width() > 434) {
-    if (x<1) {
-      x=x+1209;
-    }else if (x>300) {
-      x=x-x+1208;
+    if ($(window).width() > 434) {
+      if (x<1) {
+        x=x+1209;
+      }else if (x>300) {
+        x=x-x+1208;
+      }
+    } else {
+      if (x<1) {
+        x=x+609;
+      } else if (x>150){
+        x=x-x+608;
+      }
     }
-  } else {
-        if (x<1) {
-          x=x+609;
-        } else if (x>150){
-          x=x-x+608;
-        }
-  }
 
     $("#imgs").animate({left:-x},0);
   });
+});
+// Får modalen
+var modal = document.getElementById("myModal");
+
+// Får bilden klistrar in den i modalen
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+$("img").click(function() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+});
+
+// Span knappen som gör så man kan stänga modal
+var span = document.getElementsByClassName("close")[0];
+
+// När man trycker på span X kommer man kommer modal att stängas
+$("span").click(function () {
+  modal.style.display = "none";
 });
